ff7bcc654cd6f7f64c3fd2faf0bc3066827dd783
Cristina Rodriguez <yosola@gmail.com>
2016-07-26 16:31:14 -0700

    Passing in variables and templates tags

--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'now':datetime.now(),
+    })
