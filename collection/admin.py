f4e03bdcdecfac5af039777115dae6dbec31e9b9
Cristina Rodriguez <yosola@gmail.com>
2016-09-06 21:58:27 -0700

    Now the models allows me using several pictures and descriptions for products

--- collection/admin.py
+++ collection/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from collection.models import Products
+from collection.models import Products, ProductPicture
 from django.contrib.sites.requests import RequestSite
+
+class ProductPictureInline(admin.TabularInline):
+    model = ProductPicture
 
 
 class ProductsAdmin(admin.ModelAdmin):
     model = Products
     list_display = ('name', 'description',)
     prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductPictureInline,]
+
+class ProductPictureAdmin(admin.ModelAdmin):
+    model = ProductPicture
+    list_display = ('product', 'image',)
+
 
 admin.site.register(Products, ProductsAdmin)
+admin.site.register(ProductPicture, ProductPictureAdmin)
--- collection/models.py
+++ collection/models.py
@@ -6,5 +6,16 @@ class Products(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     slug = models.SlugField(unique=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    main_picture = models.ForeignKey('ProductPicture', blank=True, null=True)
+    overview = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class ProductPicture(models.Model):
     image = models.ImageField(upload_to='images')
-        
+    product = models.ForeignKey('Products', related_name='pictures')
+
+    def __str__(self):
+        return '%s (%s)' %(self.product, self.image)
